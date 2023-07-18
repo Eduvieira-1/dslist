9a6503765c39b732fea8e7d0466794e9cfb6ce37
Eduvieira-1 <edu.vieira1@icloud.com>
2023-07-18 15:18:58 -0300

    Sql queries in Spring Data JPA, Projections

--- src/main/java/com/dudusuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/dudusuperior/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.dudusuperior.dslist.dto;
 
 import com.dudusuperior.dslist.entities.Game;
+import com.dudusuperior.dslist.projections.GameMinProjection;
 import jakarta.persistence.Column;
 
 public class GameMinDTO {
@@ -20,6 +21,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
--- src/main/java/com/dudusuperior/dslist/services/GameService.java
+++ src/main/java/com/dudusuperior/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.dudusuperior.dslist.services;
 import com.dudusuperior.dslist.dto.GameDTO;
 import com.dudusuperior.dslist.dto.GameMinDTO;
 import com.dudusuperior.dslist.entities.Game;
+import com.dudusuperior.dslist.projections.GameMinProjection;
 import com.dudusuperior.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,6 +26,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId){
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
