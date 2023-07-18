ef79ee8c0a8901396601a3460e5fc7657f3d646f
Eduvieira-1 <edu.vieira1@icloud.com>
2023-07-18 11:44:44 -0300

    GET game id

--- src/main/java/com/dudusuperior/dslist/controller/GameController.java
+++ src/main/java/com/dudusuperior/dslist/controller/GameController.java
@@ -1,10 +1,12 @@
 package com.dudusuperior.dslist.controller;
 
+import com.dudusuperior.dslist.dto.GameDTO;
 import com.dudusuperior.dslist.dto.GameMinDTO;
 import com.dudusuperior.dslist.entities.Game;
 import com.dudusuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +18,12 @@ public class GameController {
 
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<GameMinDTO> findAll(){
